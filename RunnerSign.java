b0b5fc6d4eda11693d8e4b167155f65c206f4bd0
Pratik Kulkarni <124755862+Pratikulkarni@users.noreply.github.com>
2025-03-30 18:57:38 +0530

    Add files via upload

---
+++ RunnerSign.java
@@ -0,0 +1,23 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+	
+	@RunWith(Cucumber.class)
+
+	
+	@CucumberOptions(
+	features = {"D:\\eclipse\\LinkedIn\\Sign.feature"},
+	glue = {"StepDefination"},
+	dryRun = false,
+	monochrome= true,
+    plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+	)
+	
+			
+	public class RunnerSign {
+
+}
